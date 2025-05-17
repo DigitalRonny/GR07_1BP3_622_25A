97e5edcab7e7a080920b4997e8c093465aeb85a8
Ronny Cartagena <rondamian04@gmail.com>
2025-05-17 13:20:51 -0500

    Cambios en el calculatorExecute

--- src/main/java/ec/epn/edu/calculator/CalculatorExecute.java
+++ src/main/java/ec/epn/edu/calculator/CalculatorExecute.java
@@ -5,13 +5,21 @@ public class CalculatorExecute {
         System.out.println("==== Welcome to Calculator Execute ====");
         Calculator calc = new Calculator();
 
+        System.out.println("=== Welcome to Calculator Execute ===");
+        System.out.println(" ");
+
+        System.out.println("===ADDITION===");
         int add = calc.add(1, 2);
         System.out.println("Addition: 1 + 2 = "+ add);
 
+        System.out.println("===DIVISON===");
         int division = calc.division(30,3);
         System.out.println("Division: 30 / 3 = "+ division);
 
+        System.out.println("===MULTIPLICATION===");
         int multiplication = calc.multiplication(10,2);
         System.out.println("Multiplication: 10 * 2 = "+ multiplication);
+
+
     }
 }
